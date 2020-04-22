1ddcffa0511908c307de3869535f40894108b093
Taylor Buckner <taylora.buckner@gmail.com>
2020-04-22 15:28:45 -0500

    Following support/index.ts instructions.

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -2,4 +2,5 @@ it("loads examples", () => {
   const baseUrl: string = "http://localhost:4200";
   cy.visit(baseUrl);
   cy.contains("cypress-typescript-custom-commands app is running!");
+  cy.customCommand('test');
 });
